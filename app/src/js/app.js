4c268cfe1998a16e6b2283a6056aea7528cb6f1e
DanielRubio2401 <daniel@mingadigital.com>
2017-10-03 18:37:32 -0300

    update

---
+++ app/src/js/app.js
@@ -0,0 +1,37 @@
+﻿(function () {
+	var env = {};
+
+    'use strict';
+
+    angular
+        .module('Inventary', ['ngRoute', 'ngCookies'])
+        .config(config)
+        .run(run);
+
+
+        config.$inject = ['$routeProvider', '$location', '$http'];
+        function config($routeProvider, $locationProvider, $httpProvider) {
+            $routeProvider
+            .when('/', {
+                controller: 'HomeController',
+                templateUrl: 'views/home/home.view.html'
+            })
+            .when('/product', {
+                    controller: 'ProductController',
+                    templateUrl: 'views/product/product.view.html'
+            })
+            .otherwise({ redirectTo: '/' });
+        }   
+
+    run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];    
+    function run($rootScope, $location, $cookieStore, $http) {
+        // carga inicial
+        console.log('sii');
+        console.log('rootScope', $rootScope);
+        console.log('location', $location);
+        console.log('cookieStore', $cookieStore);
+    }
+
+
+
+})();
---
+++ app/src/js/services/flash.service.js
@@ -0,0 +1,70 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('Inventary')
+        .factory('FlashService', FlashService);
+
+    FlashService.$inject = ['$rootScope'];
+    function FlashService($rootScope) {
+        var service = {};
+
+        service.Success = Success;
+        service.Error = Error;
+
+        initService();
+
+        return service;
+
+        function initService() {
+            $rootScope.$on('$locationChangeStart', function () {
+                clearFlashMessage();
+            });
+
+            function clearFlashMessage() {
+                var flash = $rootScope.flash;
+                if (flash) {
+                    if (!flash.keepAfterLocationChange) {
+                        delete $rootScope.flash;
+                    } else {
+                        // only keep for a single location change
+                        flash.keepAfterLocationChange = false;
+                    }
+                }
+            }
+        }
+
+        function Success(message, keepAfterLocationChange) {
+            $rootScope.flash = {
+                message: message,
+                type: 'success', 
+                keepAfterLocationChange: keepAfterLocationChange
+            };
+        }
+
+        function Error(response, keepAfterLocationChange) {
+            var error_message = 'Error: ';
+            if (typeof(response.message) == 'Object' || typeof(response.message) == 'object') {
+                for (var k in response.message) {
+                	if (response.message.hasOwnProperty(k)) {
+                		var key = k !== 'error' ? k : '';
+                		error_message += key + ' ' + response.message[k] + "\n";
+                	}
+                }
+            } else {
+            	error_message += response.message;
+            }
+
+            $rootScope.flash = {
+            	message: error_message,
+                type: 'error',
+                keepAfterLocationChange: response.code == 401 ? true : keepAfterLocationChange
+            };
+                
+            if (response.code == 401) {
+             	window.location = "/login";
+            }
+        }
+    }
+
+})();
